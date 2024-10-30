35f1e2a0d86cf37defc1746226d056e2c6b404a5
Phillip Patrick <phillip.n.patrick@gmail.com>
2024-10-30 13:16:47 -0600

    easy solution to integer to roman

--- Solutions/IntegerToRoman.go
+++ Solutions/IntegerToRoman.go
@@ -1,68 +1,17 @@
 package Solutions
 
-import (
-	"fmt"
-	"math"
-	"strconv"
-	"strings"
-)
-
 func intToRoman(num int) string {
-	roman := ""
-	symbols := map[int]string{
-		1:    "I",
-		5:    "V",
-		10:   "X",
-		50:   "L",
-		100:  "C",
-		500:  "D",
-		1000: "M",
-	}
-	subtractiveForm := map[int]string{
-		4:   "IV",
-		9:   "IX",
-		40:  "XL",
-		90:  "XC",
-		400: "CD",
-		900: "CM",
-	}
-
-	for q := len(strconv.Itoa(num)) - 1; num > 0; q-- {
-		u := int(math.Pow10(q))
-		p := u * (num / u)
-
-		num -= p
-		temp := ""
-
-		if strings.HasPrefix(strconv.Itoa(p), "4") || strings.HasPrefix(strconv.Itoa(p), "9") {
-			temp = subtractiveForm[p]
-		} else if value, exists := symbols[p]; exists {
-			temp = value
+	n := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	s := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	i := 0
+	str := ""
+	for num > 0 {
+		if num >= n[i] {
+			str = str + s[i]
+			num -= n[i]
 		} else {
-			for p > 0 {
-				symbolIndex := closestValue(p)
-				temp = fmt.Sprintf("%s%s", temp, symbols[symbolIndex])
-				p -= symbolIndex
-			}
-		}
-
-		roman = fmt.Sprintf("%s%s", roman, temp)
-	}
-
-	return roman
-}
-
-func closestValue(target int) int {
-	values := []int{1, 5, 10, 50, 100, 500, 1000}
-	closest := values[0]
-	minDiff := math.Abs(float64(closest - target))
-
-	for _, value := range values[1:] {
-		diff := math.Abs(float64(value - target))
-		if diff < minDiff && value <= target {
-			minDiff = diff
-			closest = value
+			i++
 		}
 	}
-	return closest
+	return str
 }
